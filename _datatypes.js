faf4f97e45ad4bdd8549facad410126b8c016137
jaswnt3700-wq <jaswnt3700@gmail.com>
2026-03-22 05:52:30 +0000

    variables

---
+++ _datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using NodeJs. Not browser
+
+console.log(3+3); console.log("jaswnt") // not a good practice
+// priority should be code readability
+
+
+// number => 2 to power 53
+// bigint
+// string=> ""
+// boolean=> true/false
+// null // a standalone value // is also a data type
+// undefined => 
+// symbol=> unique
+// object
+
+console.log(typeof("jaswnt"))
---
+++ _variables.js
@@ -0,0 +1,16 @@
+const accountsId = 12345
+let UserEmail = "123@gmail.com"
+var Userpassword = "1234"
+userlocation = "Delhi"
+
+
+
+//accountsId=6789   //const value can not be changed / not allowed in java script
+ 
+console.log(accountsId);
+UserEmail="hc@hc.com"
+Userpassword="44566"
+userlocation="Punjab"
+let Userpreferance;
+
+console.table([UserEmail, Userpassword, userlocation, Userpreferance])
