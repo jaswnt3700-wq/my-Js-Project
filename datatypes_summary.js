fcb00045d492561cc4da2486917a0d293eb91532
jaswnt3700-wq <jaswnt3700@gmail.com>
2026-03-27 09:07:39 +0000

    datatype Summary

---
+++ datatypes_summary.js
@@ -0,0 +1,58 @@
+// Two types of Data types //
+
+// Primitives & Non-Primitives(reference type)
+
+//#Primitives (7) types: String, Number, Boolean, null(empty not zero), undefined(Only defined variable) Symbol(Unique) Bigint
+
+
+// #Non- Primitive or Reference Type Data types
+
+
+// JS is dynamically types language.
+
+const score = 100
+const scoreValue= 200.4
+const isLoggedIn = false
+const outsideTemp= null
+let userEmail;
+
+const id = Symbol('1234')
+const anotherId= Symbol('1234')
+
+console.log(id===anotherId);
+
+const bigNumber=123456789987456321n  // Bigint consider
+
+
+
+
+
+//Array, Objects, functions
+
+
+const heros = ["Shakrtiman", "Gangadhar", "Superman"];
+
+{
+    name: "Jaswnt";
+    age : 35;
+
+}
+
+const myFunction = function(){
+    console.log("Hello jaswnt")
+
+}
+console.log(typeof heros)
+
+console.log(typeof myFunction)
+
+
+// Objects & browser event most important topics
+
+
+
+
+
+
+
+
